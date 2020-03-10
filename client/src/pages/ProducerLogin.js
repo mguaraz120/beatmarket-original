3f45eefb786fa8f293842bffe9fe5f9b2ef561e6
Mario Vizcaino <vizhar1952@gmail.com>
2020-03-10 17:37:43 -0400

    Less hard coded producer login.

--- client/src/pages/ProducerLogin.js
+++ client/src/pages/ProducerLogin.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container } from "../components/Grid";
 import { Input } from "../components/Form";
+import { Link } from "react-router-dom";
+import API from "../utils/API";
 
-const ProducerLogin = () => {
-  return (
-    <form>
-      <Container>
-        <p>Username</p>
-        <Input />
+// convert to class
+// state producerId
+// API get first producers id
+// name method loadFirstProducersId()
+// Use Link component link Home Component beatsbyproducer
+class ProducerLogin extends Component {
+  state = {
+    producerId: []
+  };
 
-        <p>Password</p>
-        <Input />
-        <a href="/produceradmin/2" className="btn btn-primary" role="button">
-          Producer Login
-        </a>
-      </Container>
-    </form>
-  );
-};
+  componentDidMount() {
+    this.loadBeats();
+  }
+
+  loadFirstProducersId = () => {
+    API.getProducers()
+      .then(res => {
+        console.log(`res: ${res}`);
+        console.log(`res.data: ${res.data}`);
+        this.setState({ producerId: this.firstProducersId });
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <form>
+        <Container>
+          <p>Username</p>
+          <Input />
+
+          <p>Password</p>
+          <Input />
+
+          <Link to={"/producerAdmin/" + this.state.producerId}>
+            Producer Login
+          </Link>
+        </Container>
+      </form>
+    );
+  }
+}
 export default ProducerLogin;
