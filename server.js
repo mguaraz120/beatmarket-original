77e86adbd42a0878713a16ea6f23485713e70902
Mario Vizcaino <vizhar1952@gmail.com>
2020-03-11 14:47:22 -0400

    changed var  fro const

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
 const dbUri = "mongodb://localhost/beatmarketdb";
 mongoose.connect(process.env.MONGODB_URI || dbUri);
 let gfs;
-var conn = mongoose.createConnection(process.env.MONGODB_URI || dbUri, {
+const conn = mongoose.createConnection(process.env.MONGODB_URI || dbUri, {
   useNewUrlParser: true
 });
 conn.once("open", function() {
