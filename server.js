3c3f224b3884be6d092c31bec843b5ca31014eb4
Mario Vizcaino <vizhar1952@gmail.com>
2020-03-11 21:08:04 -0400

    Implement aws audio.

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const multer = require("multer");
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const keys = require("./constants/keys");
+
 const mongoose = require("mongoose");
 // const routes = require("./routes");
 
 const path = require("path");
 const bodyParser = require("body-parser");
-const multer = require("multer");
 
 const beatsController = require("./controllers/beatsController");
 const producersController = require("./controllers/producersController");
@@ -38,6 +42,82 @@ const storage = multer.diskStorage({
   }
 });
 const upload = multer({ storage });
+
+AWS.config.update({
+  accessKeyId: keys.iam_access_id,
+  secretAccessKey: keys.iam_secret,
+  region: keys.bucket_region
+});
+
+const s3 = new AWS.S3();
+
+app.post("/post_file", upload.single("file"), function(req, res) {
+  uploadFile(req.file.path, req.file.filename, res);
+});
+
+app.get("/get_file/:file_name", (req, res) => {
+  retrieveFile(req.params.file_name, res);
+});
+
+// https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/requests-using-stream-objects.html
+app.get("/play_file/:filename", (req, res) => {
+  const getParams = {
+    Bucket: keys.bucket_name,
+    Key: req.params.filename
+  };
+  let s3Stream = s3.getObject(getParams).createReadStream();
+  s3Stream.on("error", err => {
+    console.error(err);
+  });
+
+  s3Stream
+    .pipe(res)
+    .on("error", err => {
+      console.error("File Stream:", err);
+    })
+    .on("close", () => {
+      console.log("Done.");
+    });
+});
+
+function uploadFile(source, targetName, res) {
+  console.log("preparing to upload...");
+  fs.readFile(source, function(err, filedata) {
+    if (!err) {
+      const putParams = {
+        Bucket: keys.bucket_name,
+        Key: targetName,
+        Body: filedata
+      };
+      s3.putObject(putParams, function(err, data) {
+        if (err) {
+          console.log("Could not upload the file", err);
+          return res.send({ success: false });
+        } else {
+          fs.unlink(source, () => {});
+          console.log("Successfully uploaded the file");
+          return res.send({ success: true });
+        }
+      });
+    } else {
+      console.log({ err: err });
+    }
+  });
+}
+
+function retrieveFile(filename, res) {
+  const getParams = {
+    Bucket: keys.bucket_name,
+    Key: filename
+  };
+  s3.getObject(getParams, function(err, data) {
+    if (err) {
+      return res.status(400).send({ success: false, err: err });
+    } else {
+      return res.send(data.Body);
+    }
+  });
+}
 
 // -------------------------------------------------------
 // Beat Routing
