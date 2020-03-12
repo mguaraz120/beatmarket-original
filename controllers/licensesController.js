59b43fd06c22477a0fa7a4c8e9b397a7b22085f3
Mario Vizcaino <vizhar1952@gmail.com>
2020-03-11 20:46:24 -0400

    Fix license controller.

--- controllers/licensesController.js
+++ controllers/licensesController.js
@@ -2,28 +2,28 @@ const db = require("../models");
 
 module.exports = {
   findAll: function(req, res) {
-    db.Book.find(req.query)
+    db.License.find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book.findById(req.params.id)
+    db.License.findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book.create(req.body)
+    db.License.create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book.findOneAndUpdate({ _id: req.params.id }, req.body)
+    db.License.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book.findById({ _id: req.params.id })
+    db.License.findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
